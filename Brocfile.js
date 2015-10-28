1c78175c966f4ac8317be55a389b67b995ed61d8
Chancey Ward <chancey_ward@yahoo.com>
2015-10-28 13:40:49 -0500

    finished merge layouts

---
+++ Brocfile.js
@@ -0,0 +1,13 @@
+var Merge = require('broccoli-merge-trees');
+
+var Sass = require('broccoli-sass');
+
+var stylePaths = [
+  'sass',
+  'bower_components/normalize-css',
+  'bower_components/font-awesome/scss',
+];
+
+var styles = new Sass(stylePaths, 'app.scss', 'app.css');
+
+module.exports = new Merge(['public', styles, 'bower_components/font-awesome/fonts'], { overwrite: true });
